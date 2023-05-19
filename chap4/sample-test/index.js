d0716d168ec5c9b1c7e2a622ad19d35e99857110
kywu9232 <kywu9232@naver.com>
2023-05-20 05:12:31 +0900

    0520

---
+++ chap4/sample-test/index.js
@@ -0,0 +1,9 @@
+const calc = require("sample-package");
+
+const a = 15;
+const b = 3;
+
+console.log("a+b =", calc.add(a, b));
+console.log("a-b =", calc.sub(a, b));
+console.log("a*b =", calc.multi(a,b));
+console.log("a/b =", calc.div(a,b));
